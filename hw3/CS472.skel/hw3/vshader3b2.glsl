c8b8b926b053b83368145382189d57ec46a9bde1
MD Rahman <mdrahman1472@yahoo.com>
2017-05-01 23:23:58 -0400

    vshader3b2 fixed

--- hw3/CS472.skel/hw3/vshader3b2.glsl
+++ hw3/CS472.skel/hw3/vshader3b2.glsl
@@ -11,10 +11,6 @@ uniform mat4 u_Projection;
 void main() 
 {
 	// PUT YOUR CODE HERE
-    //gl_Position = u_Projection* u_View * vec4 (a_Position,1);
-    // Output position of the vertex, in clip space : MVP * position
     gl_Position =  u_Projection * u_View * vec4(a_Position, 1.0);
-    
-    
-    
+
 }
